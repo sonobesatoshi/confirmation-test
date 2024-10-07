27b15f1ddee691e332f93ca42c75be76c6100aa9
sonobe satoshi <a0mj4d06@gmail.com>
2024-10-07 21:36:11 +0900

    サンプルファイルの追加

---
+++ src/database/migrations/2024_09_22_222159_create_contacts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+                $table->string('name');
+                $table->tinyInteger('gender');
+                $table->string('email');
+                $table->string('tel');
+                $table->string('address');
+                $table->string('building');
+                $table->string('select');
+                $table->text('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
